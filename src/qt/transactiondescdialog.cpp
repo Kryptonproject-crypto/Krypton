440104b0b5195e6e0c0223ec42d3bd53ae43b0d5
Kryptonproject-crypto <krypton-project@outlook.fr>
2025-09-23 12:35:48 +0200

    Update transactiondescdialog.cpp

--- src/qt/transactiondescdialog.cpp
+++ src/qt/transactiondescdialog.cpp
@@ -9,14 +9,31 @@
 
 #include <QModelIndex>
 
+#include <QDesktopServices>
+
+#include <QUrl>
+
 TransactionDescDialog::TransactionDescDialog(const QModelIndex &idx, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TransactionDescDialog)
 {
     ui->setupUi(this);
-    setWindowTitle(tr("Details for %1").arg(idx.data(TransactionTableModel::TxHashRole).toString()));
-    QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
-    ui->detailText->setHtml(desc);
+    QString txid = idx.data(TransactionTableModel::TxHashRole).toString();
+setWindowTitle(tr("Details for %1").arg(txid));
+
+// lien vers l'explorateur
+QString url = "https://explorer.krypton-project.com/search.php?q=" + txid;
+
+// récupère la description longue (comme avant)
+QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
+
+// ajoute le lien cliquable au-dessus de la description
+QString html = QString("<b>Explorer link:</b> <a href=\"%1\">%2</a><br><br>%3")
+        .arg(url, txid, desc);
+
+ui->detailText->setOpenExternalLinks(true);
+ui->detailText->setTextInteractionFlags(Qt::TextBrowserInteraction);
+ui->detailText->setHtml(html);
 }
 
 TransactionDescDialog::~TransactionDescDialog()
